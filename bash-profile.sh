e9c8cb8d6a9c8fd150e1b543872da5027d655625
Jake Coxon <jake.coxon@gmail.com>
2020-07-27 17:02:20 +0100

    Changes

--- bash-profile.sh
+++ bash-profile.sh
@@ -10,6 +10,37 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 source $DIR/bash-prompt.sh
 
+export ANDROID_NDK=/Users/jake/lib/android-ndk-r10e
+export ANDROID_HOME=/Users/jake/lib/android-sdk-macosx
+
+export WORKON_HOME=/Users/jake/.virtualenvs
+
+export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:/Users/jake/lib:/usr/local/bin:$HOME/.fastlane/bin
+
+source /Users/jake/Library/Python/2.7/bin/virtualenvwrapper.sh
 
 # ll lists files with colours and sorts directories first
 function ll () { CLICOLOR_FORCE=true ls -lahG "$@" | sort -k 1.1,1.1r -k 9 -f | more -R ; }
+
+export PATH="$HOME/.cargo/bin:$PATH"
+export PATH="$HOME/.nimble/bin:$PATH"
+
+
+alias hh=hstr                    # hh to be alias for hstr
+export HSTR_CONFIG=hicolor       # get more colors
+shopt -s histappend              # append new history items to .bash_history
+export HISTCONTROL=ignorespace   # leading space hides commands from history
+export HISTFILESIZE=10000        # increase history file size (default is 500)
+export HISTSIZE=${HISTFILESIZE}  # increase history size (default is 500)
+# ensure synchronization between Bash memory and history file
+export PROMPT_COMMAND="history -a; history -n; ${PROMPT_COMMAND}"
+# if this is interactive shell, then bind hstr to Ctrl-r (for Vi mode check doc)
+if [[ $- =~ .*i.* ]]; then bind '"\C-r": "\C-a hstr -- \C-j"'; fi
+# if this is interactive shell, then bind 'kill last command' to Ctrl-x k
+if [[ $- =~ .*i.* ]]; then bind '"\C-xk": "\C-a hstr -k \C-j"'; fi
+
+
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -3,3 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -s $DIR/bash-profile.sh ~/.bash_profile
 ln -s $DIR/bashrc.sh ~/.bashrc
+ln -s $DIR/init/vscode.json "/Users/jake/Library/Application Support/Code/User/settings.json"

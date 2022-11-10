fede8428c736071479a75161b96c82d3ad7bdbe2
Jake Coxon <jake.coxon@gmail.com>
2022-11-10 16:15:34 +0000

    zsh

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ln -s $DIR/bash-profile.sh ~/.bash_profile
-ln -s $DIR/bashrc.sh ~/.bashrc
+# ln -s $DIR/bash-profile.sh ~/.bash_profile
+# ln -s $DIR/bashrc.sh ~/.bashrc
+ln -s $DIR/zshrc.sh ~/.zshrc
 ln -s $DIR/init/vscode.json "/Users/$USER/Library/Application Support/Code/User/settings.json"
 ln -s $DIR/init/keybindings.json "/Users/$USER/Library/Application Support/Code/User/keybindings.json"
